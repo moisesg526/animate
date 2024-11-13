346b32e65b2b4e84d118815bfc0aa78c63cff8c1
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-11-12 23:57:39 -0500

    Added css to randomize button. Also added a hero pitch and added the website logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,17 @@ function Hero() {
   return (
     <div className="hero">
       <div className="top-header">
-        <img src="animate.svg" alt="Animate Logo" className="logo"/>
+        <img src="/images/AniMate.png" alt="logo" className="logo" />
         <div className="right-top-header">
-          <h2>My LIST</h2>
+          <h2>MY LIST</h2>
+          <button className="randomize-btn">Randomize</button>
         </div>
+      </div>
+      <div className="hero-pitch">
+        <h3>
+          Not sure what to watch next? How about we select a random Anime for
+          you and your friends.
+        </h3>
       </div>
     </div>
   );
