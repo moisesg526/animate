35d44f67cd8bc36b9d2b563ae6d539ca29aa7416
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-11-13 23:17:23 -0500

    Created a suggestions file with its css file. Also changed some of the padding on the hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Hero from "./components/Hero";
+import Suggestions from "./components/Suggestions";
 
 function App() {
   return (
     <>
       <Hero />
+      <Suggestions />
     </>
   );
 }
---
+++ src/components/Suggestions.jsx
@@ -0,0 +1,10 @@
+import "../styles/suggestions.css";
+
+function Suggestions() {
+  return (
+    <div className="suggestions-body">
+        <h2>How About a Few Suggestions</h2>
+    </div>
+  )
+}
+export default Suggestions;
