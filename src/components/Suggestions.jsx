7dc82c70ea611f53b9fdd09631cce0497ecfa28f
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-11-14 22:16:57 -0500

    Updated suggestions component to fetch data from api by using useEffect and useState. Added a card div in the suggestions component that renders an anime image, title, and synopsis.

--- src/components/Suggestions.jsx
+++ src/components/Suggestions.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import "../styles/suggestions.css";
 
 function Suggestions() {
+  const randomAnimeUrl = "https://api.jikan.moe/v4/random/anime";
+
+  const [anime, setAnime] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(randomAnimeUrl);
+      const data = await response.json();
+      setAnime(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="suggestions-body">
-        <h2>How About a Few Suggestions</h2>
+      <h2>How About a Few Suggestions</h2>
+      <div className="suggestion-card">
+        <div className="card-img">{anime && <img src={anime.data.images.jpg.image_url}/>}</div>
+        <div className="title">{anime && <h3>{anime.data.title}</h3>}</div>
+        <div className="synopsis">{anime && <p>{anime.data.synopsis}</p>}</div>
+      </div>
     </div>
-  )
+  );
 }
 export default Suggestions;
