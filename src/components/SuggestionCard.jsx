cfa43fbea887578247d495110e80d8487fefde58
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-11-23 23:56:56 -0500

    Added a useState and ternary operator to toggle the like button

--- src/components/SuggestionCard.jsx
+++ src/components/SuggestionCard.jsx
@@ -1,9 +1,20 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 function SuggestionCard({ cardImg, title, synopsis }) {
+  let heart = <i className="fa-solid fa-heart like-btn"></i>;
+  let solidHeart = <i className="fa-solid fa-heart"></i>;
+
+  const [like, setLike] = useState(heart);
+  const handleClick = () => {
+    setLike(like ? heart : solidHeart);
+  };
   return (
     <div className="suggestion-card">
       {/* <i className="fa-solid fa-heart like-btn"></i> */}
+      <div className="like" onClick={handleClick}>
+        {like}
+      </div>
       <i className="fa-regular fa-heart like-btn"></i>
       <div className="card-img">{cardImg}</div>
       <div className="title">
